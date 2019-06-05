12b17bcd48d01c7946aa824e8d53ae551090e892
simo38c8 <44566499+simo38c8@users.noreply.github.com>
2019-06-05 18:50:12 +0200

    Singleview ændringer, størrelse og padding tilføjet
    Singleview ændringer, størrelse og padding tilføjet

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.addEventListener("load", sidenVises);
             //klik på topmenu-knap
             document.querySelector("#menuknap").addEventListener("click", kryds);
             document.querySelector("#menuknap").addEventListener("click", toggletopmenu);
-            document.querySelector("#dropdownmobil").addEventListener("click", toggledropdown);
+            
             
 
 } /*Denne funktion sætte eventlistener på navigastionsbaren og bekræfter vinduet bliver loadet korrekt.*/
@@ -22,30 +22,3 @@ function kryds(){
     let element = document.getElementById("menuknap");
     element.classList.toggle("change");
 }
-
-
-         function toggledropdown() {
-             
-console.log("funktion toggledropdown virker");
-              
-  let work = document.getElementById("dropdown-contentmobil");
-        
-             
-  if (work.style.display === "block") {
-    work.style.display = "none";
-  } else {
-    work.style.display = "block";
-          
-}
-   
-  }
-
-
-   
-
-
-
-
-
-
-
