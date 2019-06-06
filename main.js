6c6b73f402267aaff932929411cea69c31eaa75c
simo38c8 <44566499+simo38c8@users.noreply.github.com>
2019-06-06 14:27:24 +0200

    Burger menu skifter nu farve ifht. forsiden
    Burger menu skifter nu farve ifht. forsiden

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-window.addEventListener("load", sidenVises);
-        function sidenVises() {
-            console.log("siden vises");
-            //klik på topmenu-knap
-            document.querySelector("#menuknap").addEventListener("click", kryds);
-            document.querySelector("#menuknap").addEventListener("click", toggletopmenu);
-            
-            
-
-} /*Denne funktion sætte eventlistener på navigastionsbaren og bekræfter vinduet bliver loadet korrekt.*/
-
-        function toggletopmenu() {
-            console.log("toggle topmenu");
-          document.querySelector("#topmenu").classList.toggle("hidden");
-            document.querySelector('.bar1').style.backgroundColor = "black";
-            document.querySelector('.bar2').style.backgroundColor = "black";
-            document.querySelector('.bar3').style.backgroundColor = "black";
 
 
-        } /*Her bliver burgermenuen tooglet fra og til*/
 
-    
-function kryds(){
-    console.log("funktion kryds virker");
-    let element = document.getElementById("menuknap");
-    element.classList.toggle("change");
-}
+
